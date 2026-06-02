fc582424af54ef8470d97ab8541cc5d286c72064
Aquiteus <myname7777@yahoo.com>
2026-06-02 09:40:12 -0400

    slight improvement to security, new card more dedicated to it was promised later

--- backend/userService/src/main/java/com/bank/userservice/SecurityConfig.java
+++ backend/userService/src/main/java/com/bank/userservice/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
@@ -21,8 +23,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
-                );
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                        "/api/auth/register", "/api/auth/login").permitAll());
         return http.build();
     }
 }
